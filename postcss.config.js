27f37652f9cf02f35bc1d6177b86b49953a7cb82
ThiagoBrunoPereira <thiagomeezz@gmail.com>
2023-04-13 11:57:04 -0300

    refactor(postcss): add new config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('postcss-import')(),
+    require('tailwindcss/nesting'),
+    require('tailwindcss')('./tailwind.config.cjs'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+  ],
+};
